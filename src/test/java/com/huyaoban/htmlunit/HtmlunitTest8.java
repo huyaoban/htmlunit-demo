3e10cfdd14fc57384279344fb090eae885f782e7
huyaoban <huyaoban@163.com>
2019-08-16 16:44:39 +0800

    fix:站内信html特殊字符需要转义后发送

--- src/test/java/com/huyaoban/htmlunit/HtmlunitTest8.java
+++ src/test/java/com/huyaoban/htmlunit/HtmlunitTest8.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.HtmlUtils;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.HttpMethod;
@@ -28,25 +29,32 @@ public class HtmlunitTest8 {
 	@Autowired
 	private WebClientProvider webClientProvider;
 
-	@Test
+	// @Test
 	public void test1() throws FailingHttpStatusCodeException, IOException {
 		WebClient client = webClientProvider.getWebClient();
 
 		WebRequest req = new WebRequest(new URL(
-				"https://sellercentral.amazon.com/messaging/api/orders/111-7121530-7757804/contactBuyer?marketplaceId=ATVPDKIKX0DER&buyerId=&customerType=&isReturn=false&documentReferrer="));
+				"https://sellercentral.amazon.com/messaging/api/orders/111-6299753-5976241/contactBuyer?marketplaceId=ATVPDKIKX0DER1&buyerId=&customerType=&isReturn=false&documentReferrer="));
 		req.setHttpMethod(HttpMethod.POST);
 		req.setAdditionalHeader("Content-Type", "application/json");
 		// 发送亚马逊站内信要带上这个请求头
 		req.setAdditionalHeader("X-Requested-With", "A01596255MPE0V36MRSF");
 
+		// html特殊字符需要转义
 		req.setRequestBody(
-				"{\"uploadingFiles\":[],\"failedFiles\":[],\"failedFilesTooLarge\":false,\"topicId\":\"6\",\"rawMessageBody\":\"99999foweufoewflajfjsofuew77777\",\"attachments\":[]}");
+				"{\"attachments\":[],\"failedFiles\":[],\"failedFilesTooLarge\":false,\"rawMessageBody\":\"45&deg;\",\"topicId\":\"6\",\"uploadingFiles\":[]}");
 
 		Page amazonMsgPage = client.getPage(req);
 		WebResponse resp = amazonMsgPage.getWebResponse();
 		log.info("{}", resp.getContentAsString(Charset.forName("UTF-8")));
 
 		webClientProvider.returnWebClient(client);
+	}
+
+	@Test
+	public void test2() {
+		log.info("after escape {}", HtmlUtils.htmlEscape(
+				"the heating pad can be washed in a normal wash at max 40?/104°F; do not bleach it; do not dry this pad in a tumble drier.\\n3. The heating pad will activate level 4 heating by default when powered in and automatically turn off in 30 minutes if not set up with a timer.\\n\\nAs a growing company, we hope that you will be willing to share your experience on Amazon! "));
 	}
 
 }
